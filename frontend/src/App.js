183e61ae218b08417e6a8bc6ff905f6b47a0dce4
karlMoreno <karlcmoreno0811@gmail.com>
2024-04-20 14:41:13 -0700

    Organization for react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,27 +9,15 @@ function App() {
     fetch('http://localhost:3002/api/data') // Make sure the URL matches your Express server's address
       .then(response => response.json())
       .then(data => {
-        console.log('Movie data:', data); // Logs fetched data to the console
+        console.table('Movie data:', data); // Logs fetched data to the console
       })
       .catch(error => console.error('Error fetching data:', error));
+    console.log("Hey?")
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
