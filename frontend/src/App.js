4e782d00ac2d234fd0efb015a299e33075a337d3
karlMoreno <karlcmoreno0811@gmail.com>
2024-04-28 17:07:32 -0700

    Landing page added and routed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
-
+import LandingPage from './pages/LandingPage';
 
 
 function App() {
@@ -16,6 +16,8 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
+
       </Routes>
     </Router>
   );
