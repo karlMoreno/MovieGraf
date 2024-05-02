a68f1ceb7059219758dc8ffe8d538ae53ce204a3
karlMoreno <karlcmoreno0811@gmail.com>
2024-05-01 22:00:08 -0700

    Added anchor button compoenents to compoenents

--- frontend/src/components/dashboard/AssetsPage.jsx
+++ frontend/src/components/dashboard/AssetsPage.jsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import VirtualizedTable from './VirtualizedTable';  // Adjust the import path as necessary
-
+import VirtualizedTable from '../VirtualizedTable';  // Adjust the import path as necessary
+import AnchorButton from '../AnchorButton'
 
 export default function AssetPage() {
   const theme = useTheme();
 
   return (
-    <div style={{ fontSize: '24px', color: theme.palette.text.primary }}>
-      Assets Page
-      <div>
-        <VirtualizedTable></VirtualizedTable>
+    <div>
+      <div style={{ fontSize: '24px', color: theme.palette.text.primary }}>
+        Assets Page
       </div>
+      <AnchorButton></AnchorButton>
+      <VirtualizedTable></VirtualizedTable> // Using the table component here
     </div>
     
   );
