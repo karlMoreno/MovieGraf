34daf7e0e0305a06e235920ab66eed6fad09db81
karlMoreno <karlcmoreno0811@gmail.com>
2024-06-12 15:18:34 -0700

    Project Routes added to index via /projects

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const userRoutes = require('./UserRoutes'); // Correct capitalization
-const assetRoutes = require('./AssetRoutes'); // Correct capitalization
-const taskRoutes = require('./TaskRoutes'); // Correct capitalization
+const userRoutes = require('./UserRoutes'); 
+const assetRoutes = require('./AssetRoutes'); 
+const taskRoutes = require('./TaskRoutes'); 
+const projectRoutes = require('./ProjectRoutes');
 
 // User routes
 router.use('/user', userRoutes);
@@ -13,5 +14,8 @@ router.use('/tasks', taskRoutes);
 
 // Asset routes
 router.use('/assets', assetRoutes);
+
+//Project routes
+router.use('/projects', projectRoutes);
 
 module.exports = router;
