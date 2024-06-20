499a7495e4c5154fc367d0e5e346bb9a349274b7
karlMoreno <karlcmoreno0811@gmail.com>
2024-06-20 16:29:08 -0700

    Projects .env apis

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -40,7 +40,7 @@ export default function Projects() {
 
       try {
         const response = await fetch(
-          "http://localhost:3002/api/projects/list",
+          `${process.env.REACT_APP_API_URL}/api/projects/list`,
           {
             method: "GET",
             headers: {
@@ -88,7 +88,7 @@ export default function Projects() {
     }
 
     try {
-      const response = await fetch("http://localhost:3002/api/projects/add", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/projects/add`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
